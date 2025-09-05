8051916c60cd202434085a690462e03f2f5ab43e
Rodrigo <rodrigocardosotadeo@gmail.com>
2025-09-05 17:18:16 -0300

    adicionando a calculadora

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import "../components/Button.css";
+
+const Button = ({children, onClick, className}) => {
+  return (
+    <button className={`button ${className}`} onClick={onClick}>
+        {children}
+    </button>
+  )
+}
+
+export default Button
+
--- src/routes/CalculatorPage.jsx
+++ src/routes/CalculatorPage.jsx
@@ -1,12 +1,69 @@
-import { useForm } from "react-hook-form"
+// import { useForm } from "react-hook-form"
 import { FcRegisteredTrademark } from "react-icons/fc"
+import "./CalculatorPage.css";
+import Button from "../components/Button"
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
 
 const CalculatorPage = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (value) =>{
+    setInput((prevInput) => prevInput + value);
+  }
+
+  const handleClear = () =>{
+    setInput("");
+    setResult("");
+  }
+
+  const handleEqual = () =>{
+    try {
+      setResult(evaluate(input).toString());
+    } catch (error) {
+      setResult("Erro")
+    }
+  };
+
+
   return (
-    <section className='bg-gray-200 text-black min-h-screen flex flex-col md:flex-row items-center justify-center p-8' >
-      <div>
-        CalculatorPage
+    <section className="calculator-container"/*className='bg-gray-200 text-black min-h-screen flex flex-col md:flex-row items-center justify-center p-8'*/>
+ 
+      <div className="calculator">
+        <div className="display">
+          <div className="input">{input}</div>
+          <div className="result">{result}</div>
+        </div>
+        <div className="buttons">
+          <Button className="clear" onClick={handleClear}>
+            C
+          </Button>
+          <Button onClick={() => handleClick("(")}>(</Button>
+          <Button onClick={() => handleClick(")")}>)</Button>
+          <Button className="operator" onClick={() => handleClick("/")}>/</Button>
+
+          <Button onClick={() => handleClick("7")}>7</Button>
+          <Button onClick={() => handleClick("8")}>8</Button>
+          <Button onClick={() => handleClick("9")}>9</Button>
+          <Button className="operator" onClick={() => handleClick("*")}>*</Button>
+
+          <Button onClick={() => handleClick("4")}>4</Button>
+          <Button onClick={() => handleClick("5")}>5</Button>
+          <Button onClick={() => handleClick("6")}>6</Button>
+          <Button className="operator" onClick={() => handleClick("-")}>-</Button>
+
+          <Button onClick={() => handleClick("1")}>1</Button>
+          <Button onClick={() => handleClick("2")}>2</Button>
+          <Button onClick={() => handleClick("3")}>3</Button>
+          <Button className="operator" onClick={() => handleClick("+")}>+</Button>
+
+          <Button onClick={() => handleClick("0")}>0</Button>
+          <Button onClick={() => handleClick(".")}>.</Button>
+          <Button className="equal" onClick={handleEqual}>=</Button>
+        </div>
       </div>
+
     </section>
     
     
