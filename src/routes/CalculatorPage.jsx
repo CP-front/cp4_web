5cad741b03d4ef400ea0cefa5b6b9c401a80daa8
Breno <brenogsbao@gmail.com>
2025-09-05 10:20:33 -0300

    background calculatorpage

--- src/routes/CalculatorPage.jsx
+++ src/routes/CalculatorPage.jsx
@@ -3,7 +3,13 @@ import { FcRegisteredTrademark } from "react-icons/fc"
 
 const CalculatorPage = () => {
   return (
-    <div>CalculatorPage</div>
+    <section className='bg-gray-200 text-black min-h-screen flex flex-col md:flex-row items-center justify-center p-8' >
+      <div>
+        CalculatorPage
+      </div>
+    </section>
+    
+    
   )
 }
 
