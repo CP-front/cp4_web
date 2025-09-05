f6ffa4787c325d1e095c85cbaf45317e3c61bc3e
Breno <brenogsbao@gmail.com>
2025-09-05 00:16:31 -0300

    testando tailwind em home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,13 @@
-
-
 const Home = () => {
   return (
-    <section>
-      
+    <section className='bg-gray-900 text-white min-h-screen flex flex-col md:flex-row items-center justify-center p-8'>
+        <div className='flex-1 flex-col items-center md:items-start text-center md:text-left mb-8 md:mb-0 mr-16'>
+            <p className='text-lg text-gray-400'>Teste</p>
+            <h4 className='text-4xl sm:text-5xl md:text-6xl font-bold mt-2'>Teste<br/> Calculos</h4>
+        </div>
+        <button className='mt-8 px-6 py-3 text-white font-extrabold bg-blue-400'>
+            Calcular
+        </button>
     </section>
   )
 }
